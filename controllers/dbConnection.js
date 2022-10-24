b70535cffd4cf526102a616d6d7fffc53003854c
Blaise-Opiyo <blaiseopiyo@gmail.com>
2022-10-24 22:25:20 +0300

    configured JAWSDB mysql environment for deployment on Heroku

--- controllers/dbConnection.js
+++ controllers/dbConnection.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql');
 
-var conn = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  var conn = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  var conn = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
     database : 'eshop'
   });
+}
+
+
 
   module.exports = conn;
